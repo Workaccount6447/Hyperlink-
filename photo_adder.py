307f1ab921d962ef4c603ab3aab817b728561be5
Workaccount6447 <sndj86779@gmail.com>
2025-04-20 17:23:20 +0530

    Update photo_adder.py

--- photo_adder.py
+++ photo_adder.py
@@ -16,7 +16,7 @@ from selenium.webdriver.chrome.options import Options
 
 # --- Configuration ---
 OWNER_ID = 7588665244
-BOT_TOKEN = "7864703583:AAGqZInSK2tp8Jykwpte7Ng0iunmYLlRwms"
+BOT_TOKEN = "7903608399:AAGxBNWuFGKHUv3J0poHKR8-vd-IIC--odU"
 ACCESS_FILE = "access.json"
 HEALTH_PORT = 8000
 
