1ac5ba068ae68ed50ab4a66ef72df852834c5093
Workaccount6447 <sndj86779@gmail.com>
2025-10-01 16:22:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -205,4 +205,4 @@ def run_bot():
 
 if __name__ == "__main__":
     threading.Thread(target=run_bot).start()
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
