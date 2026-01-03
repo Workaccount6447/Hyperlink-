cdbdd514358eea34002d2d96ce6f4e9da43619a1
Workaccount6447 <sndj86779@gmail.com>
2026-01-03 13:05:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from fastapi import FastAPI, UploadFile, File, HTTPException
 from fastapi.responses import HTMLResponse, RedirectResponse, JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
 from sqlalchemy import (
     create_engine,
